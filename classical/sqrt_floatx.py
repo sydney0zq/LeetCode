bb7bd47dd731c948f973ca341e50bdc2bc19e4c1
Qiang Zhou <qiang.zhou@Qiangs-MacBook-Pro.local>
2020-08-11 01:09:03 +0800

    add sqrt(x)

---
+++ classical/sqrt_floatx.py
@@ -0,0 +1,72 @@
+import math
+
+# https://blog.csdn.net/leviopku/article/details/82811478
+
+# 二分法和牛顿法求根号是面试中的经典题，如果没提前接触过，经典题将成为经典难题
+
+
+precision = 0.0000004
+
+def sqrtx_dich(x):
+    assert x >= 1
+    miny = 0
+    maxy = x
+    mid = x/2.0
+
+    # formulation: when x-p< mid*mid < x+p, exit
+    # ------------------------- maxy
+    #
+    # ------------------------- y + precision
+    # ------------------------- y
+    # ------------------------- y - precision
+    # 
+    # ------------------------- miny
+    # But in this algorithm, x must be bigger than 1
+    # because when x < 1, sqrt(x) is not between 0 and x but sqrt(x) > x instead
+    while mid*mid > x+precision or mid*mid < x-precision:
+        mid = (miny+maxy)/2.0
+        if mid*mid > x+precision:
+            maxy = mid
+        if mid*mid < x-precision:
+            miny = mid
+
+    return mid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (sqrtx_dich(5))
+
+print (math.sqrt(5.0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
